591796ede63304276f5d9ae32c367cb0839fc227
Vlad <vladvesinin676@gmail.com>
2025-03-24 00:52:59 +0500

    structure created

---
+++ Calculator/Views/MainView.axaml.cs
@@ -0,0 +1,69 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Calculator.ViewModels;
+
+namespace Calculator.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+
+    private void clear(object? sender, RoutedEventArgs e)
+    {
+        output.Text = string.Empty;
+    }
+
+    private void zero(object? sender, RoutedEventArgs e)
+    {
+        output.Text += "0";
+    }
+
+    private void one(object? sender, RoutedEventArgs e)
+    {
+        output.Text += "1";
+    }
+
+    private void two(object? sender, RoutedEventArgs e)
+    {
+        output.Text += "2";
+    }
+
+    private void three(object? sender, RoutedEventArgs e)
+    {
+        output.Text += "3";
+    }
+
+    private void four(object? sender, RoutedEventArgs e)
+    {
+        output.Text += "4";
+    }
+
+    private void five(object? sender, RoutedEventArgs e)
+    {
+        output.Text += "5";
+    }
+
+    private void six(object? sender, RoutedEventArgs e)
+    {
+        output.Text += "6";
+    }
+
+    private void seven(object? sender, RoutedEventArgs e)
+    {
+        output.Text += "7";
+    }
+
+    private void eight(object? sender, RoutedEventArgs e)
+    {
+        output.Text += "8";
+    }
+
+    private void nine(object? sender, RoutedEventArgs e)
+    {
+        output.Text += "9";
+    }
+
+}
